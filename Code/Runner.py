db133174f704851316a3648d6c293f928c0ed570
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-04-24 00:19:59 +0700

    fuckfuck

--- Code/Runner.py
+++ Code/Runner.py
@@ -37,7 +37,7 @@ for file in ['ptlog_20181015.json']:
         line = line.replace("']", '"]')
         line = line.replace(" '", ' "')
         line = line.replace("[None,", '["None",')
-        # @Todo #1 Fix link 'https://www.google.co.th/search?q="Coleman+ecodome+5"&oq="Coleman+ecodome+5"&gs_l=mobile-heirloom-serp.12..30i10.5393.42792.0.43919.25.19.3.3.3.0.437.3699.1j11j6j0j1.19.0....0...1c.1j4.34.mobile-heirloom-serp..5.20.3101.60TxhGRMC1o' Problem
+        # @todo #1 Fix link 'https://www.google.co.th/search?q="Coleman+ecodome+5"&oq="Coleman+ecodome+5"&gs_l=mobile-heirloom-serp.12..30i10.5393.42792.0.43919.25.19.3.3.3.0.437.3699.1j11j6j0j1.19.0....0...1c.1j4.34.mobile-heirloom-serp..5.20.3101.60TxhGRMC1o' Problem
         print(line)
         data_store.append(json.loads(line))
     print("Finish")
