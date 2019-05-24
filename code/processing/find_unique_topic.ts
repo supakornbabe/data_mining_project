17453ced966c20be47da6748a5534454e75fb973
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-05-24 22:40:45 +0700

    from babe

--- code/processing/find_unique_topic.ts
+++ code/processing/find_unique_topic.ts
@@ -1,20 +1,17 @@
-import path from 'path';
-import readline from 'readline';
 import fs from 'fs';
 import fetch from 'node-fetch';
+import path from 'path';
+import readline from 'readline';
 
 (async function() {
     const topicSet = new Set<string>();
 
     await (function() {
-        const ifs = fs.createReadStream(path.resolve(`../data/processed_except/pantip_edge_10.csv`), {
-            encoding: 'utf-8'
-        });
+        const ifs = fs.createReadStream(
+            path.resolve(`../data/processed_except/pantip_edge_10.csv`),
+            {encoding : 'utf-8'});
 
-        const rl = readline.createInterface({
-            input: ifs,
-            terminal: false
-        });
+        const rl = readline.createInterface({input : ifs, terminal : false});
 
         return new Promise((resolve, reject) => {
             rl.on('line', line => {
@@ -27,39 +24,35 @@ import fetch from 'node-fetch';
         });
     })();
 
-    const topics = [...topicSet.entries()];
+    const topics = [...topicSet.entries() ];
     for (let i = 0; i < topics.length; i++) {
         const reqUrl = `https://m.pantip.com/topic/${topics[i][0]}`;
-        console.log(reqUrl);
+        // console.log(reqUrl);
 
         let res = '';
         try {
-            res = await fetch(reqUrl, {
-                method: 'get'
-            })
-            .then(res => res.text());
+            res = await fetch(reqUrl, {method : 'get'}).then(res => res.text());
         } catch (err) {
             console.error(err);
         }
-        
-        const title = (function(){
-            const matches = res.match(/<meta name="twitter:title" content="(.*)" \/>/);
+
+        const title = (function() {
+            const matches =
+                res.match(/<meta name="twitter:title" content="(.*)" \/>/);
             return matches === null ? '' : matches[1];
         })();
 
-        const date = (function(){
-            const matches = res.match(/<abbr title=".*" data-utime="(.*)" class="timeago"><\/abbr>/);
+        const date = (function() {
+            const matches = res.match(
+                /<abbr title=".*" data-utime="(.*)" class="timeago"><\/abbr>/);
             return matches === null ? '' : matches[1];
         })();
 
-        const data = { title, date };
+        const data = {title, date, URL : reqUrl, topic_id : topics[i][0]};
         console.log(JSON.stringify(data));
-        
-        await new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve();
-            }, 10);
-        });
+
+        await new Promise(
+            (resolve, reject) => { setTimeout(() => { resolve(); }, 10); });
         // console.log(res);
         // break;
     }
